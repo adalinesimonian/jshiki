7b332ff57aa2b5a491266a5623a0e0d4e484e94f
Vartan Simonian <vsimonianpress@gmail.com>
2015-09-14 04:06:04 -0700

    Fix scope test
    The variables were mismatched and should have been in the `it` blocks

--- test/functional/scope.js
+++ test/functional/scope.js
@@ -8,9 +8,6 @@ var jshiki = require('../../index')
 var jeval = (expression, scope) =>
   jshiki.parse(expression, { scope: scope }).eval()
 
-var y = '1' // eslint-disable-line no-unused-vars
-global.z = '2'
-
 describe('Scope', () => {
   it('should be able to access scoped properties', () => {
     expect(jeval('x', { x: '1' })).to.equal('1')
@@ -21,10 +18,12 @@ describe('Scope', () => {
   })
 
   it('should not be able to access global variables', () => {
+    global.y = '1'
     expect(jeval('y')).to.equal(undefined)
   })
 
   it('should not be able to access unscoped variables', () => {
+    var z = '1' // eslint-disable-line no-unused-vars
     expect(jeval('z')).to.equal(undefined)
   })
 })
